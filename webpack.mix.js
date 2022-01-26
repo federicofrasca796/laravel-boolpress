b0dd550802f876b2881d519fdcf3afc18101887f
Federico Frascà <federico.frasca796@gmail.com>
2022-01-26 16:35:03 +0100

    add custom js,sass for admin, update webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.options({
+    processCssUrls: false
+})
+    .copyDirectory('resource/img', 'public/img')
+    .js('resources/js/app.js', 'public/js')
+    .js('resources/js/admin.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/admin.scss', 'public/css');
