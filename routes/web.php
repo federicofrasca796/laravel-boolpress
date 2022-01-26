f09c0e2e8e6ed126af2b93010b321143abb6a766
Federico Frascà <federico.frasca796@gmail.com>
2022-01-26 16:13:26 +0100

    add auth facade, change home route in admin route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/admin', 'HomeController@index')->name('admin');
+
+Route::group
