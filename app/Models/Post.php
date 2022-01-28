e42b61d9e5effe3593e39d68665e2e51031498f9
Federico Frascà <federico.frasca796@gmail.com>
2022-01-28 16:37:08 +0100

    add categories/posts one-to-many relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
     protected $fillable = ['cover', 'title', 'slug', 'sub_title', 'body'];
 }
